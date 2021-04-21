ac810921e34f89275508725f8acbc01caec7b9da
RYOTA <kobaryo04ysh@icloud.com>
2021-04-21 20:18:49 +0900

    feat: authの状態を表すための関数を追加

--- auth-app-client/src/components/Home.js
+++ auth-app-client/src/components/Home.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import Registration from './auth/Registration'
 
+import axios from "axios";
 
-const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Registration  />
-        </div>
-    )
-}
+import Login from "./auth/Login";
+
+
+const Home = (props) => {
+
+    const handleSuccessfulAuthentication = (data) => {
+        props.history.push("/dashboard");
+    };
+
+    const handleLogoutClick = () => {
+        axios
+        .delete("http://localhost:3001/logout", { withCredentials: true })
+        .then((response) => {
+            props.handleLogout();
+        })
+        .catch((error) => console.log("ログアウトエラー", error));
+    };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <h2>ログイン状態: {props.loggedInStatus}</h2>
+      <button onClick={handleLogoutClick}>ログアウト</button>
+      <Registration
+        handleSuccessfulAuthentication={handleSuccessfulAuthentication}
+      />
+      <Login handleSuccessfulAuthentication={handleSuccessfulAuthentication} />
+    </div>
+  );
+};
 
 export default Home
