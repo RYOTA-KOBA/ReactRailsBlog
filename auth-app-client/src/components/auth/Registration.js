81ee8f15b92de27f29ad8a654c109e5c8227b756
RYOTA <kobaryo04ysh@icloud.com>
2021-04-21 20:19:24 +0900

    feat: ユーザー登録用の関数を追加

--- auth-app-client/src/components/auth/Registration.js
+++ auth-app-client/src/components/auth/Registration.js
@@ -1,62 +1,67 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-const Registration = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [passwordConfirmation, setPasswordConfirmation] = useState("")
-    
-    const handleSubmit = (event) => {
+const Registration = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
-        //追加
-        axios.post("http://localhost:3001/signup",
-            {
-                user: {
-                    email: email,
-                    password: password,
-                    password_confirmation: passwordConfirmation
-                }
-            },
-             { withCredentials: true }
-        ).then(response => {
-            console.log("registration res", response)
-        }).catch(error => {
-            console.log("registration error", error)
-        })
-        event.preventDefault()
+  const handleSubmit = (event) => {
+    //追加
+    axios
+      .post(
+        "http://localhost:3001/signup",
+        {
+          user: {
+            email: email,
+            password: password,
+            password_confirmation: passwordConfirmation,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log("registration res", response);
+        if (response.data.status === "created") {
+          props.handleSuccessfulAuthentication(response.data);
+        }
+      })
+      .catch((error) => {
+        console.log("registration error", error);
+      });
+    event.preventDefault();
+  };
 
-    }
+  return (
+    <div>
+      <p>新規登録</p>
 
-    return (
-        <div>
-           <p>新規登録</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="メールアドレス"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="パスワード"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <input
+          type="password"
+          name="password_confirmation"
+          placeholder="確認用パスワード"
+          value={passwordConfirmation}
+          onChange={(event) => setPasswordConfirmation(event.target.value)}
+        />
 
-           <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="メールアドレス"
-                    value={email}
-                    onChange={event => setEmail(event.target.value)}
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="パスワード"
-                    value={password}
-                    onChange={event => setPassword(event.target.value)}
-                />
-                <input
-                    type="password"
-                    name="password_confirmation"
-                    placeholder="確認用パスワード"
-                    value={passwordConfirmation}
-                    onChange={event => setPasswordConfirmation(event.target.value)}
-                />
-
-                <button type="submit">登録</button>
-            </form>
-        </div>
-    )
-}
+        <button type="submit">登録</button>
+      </form>
+    </div>
+  );
+};
 export default Registration
