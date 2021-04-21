9ee161934793f6ebe4572904c6c5fa3a3f04e11b
RYOTA <kobaryo04ysh@icloud.com>
2021-04-21 20:18:10 +0900

    feat: ログイン状態の表示

--- auth-app-client/src/components/Dashboard.js
+++ auth-app-client/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
+      <div>
+        <h1>Dashboard</h1>
+        <h2>ログイン状態: {props.loggedInStatus}</h2>
+      </div>
+    );
 }
 export default Dashboard
