be77b04e5462987c8b0a620b7f8bd9c0f1f08d7b
RYOTA <kobaryo04ysh@icloud.com>
2021-04-21 17:39:57 +0900

    feat: create-react-appでフロント部分のreactを追加

---
+++ auth-app-client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+
+const App = () => {
+  return (
+    <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/dashboard"} component={Dashboard} />
+          </Switch>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
