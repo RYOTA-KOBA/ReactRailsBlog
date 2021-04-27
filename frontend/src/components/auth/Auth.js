11d7b7d0942a5f356225e5bbe7c01980355304c3
RYOTA <kobaryo04ysh@icloud.com>
2021-04-27 21:18:37 +0900

    fix: axiosのコードをcontextに移動

--- frontend/src/components/auth/Auth.js
+++ frontend/src/components/auth/Auth.js
@@ -1,35 +1,21 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useHistory } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 const Auth = ({ children }) => {
-  const history = useHistory();
-    const [isSignedIn, setIsSignedIn] = useState(false)
-    // const [user, setUser] = useState({});
+  const { getUserData, isSignedIn } = useAuth();
 
-    useEffect(() => {
-      if (!isSignedIn) {
-          axios
-            .get("http://localhost:3001/logged_in", {
-              withCredentials: true,
-            })
-            .then((res) => {
-              if (res.data.logged_in) {
-                setIsSignedIn(true)
-                // setUser(res.data.user)
-              } else {
-                history.push('/login')
-              }
-            });
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
+  useEffect(() => {
     if (!isSignedIn) {
-        return <></>
-    } else {
-        return children
+      getUserData();
     }
-}
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-export default Auth
+  if (!isSignedIn) {
+    return <></>;
+  } else {
+    return children;
+  }
+};
+
+export default Auth;
