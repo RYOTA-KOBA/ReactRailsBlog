e07b788f1266c88ed646abdc39696a771b05d751
RYOTA <kobaryo04ysh@icloud.com>
2021-04-27 21:18:52 +0900

    feat: 関数getUserDataを追加。

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,4 +1,6 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const AuthContext = React.createContext({});
 
@@ -7,12 +9,38 @@ export const useAuth = () => {
 };
 
 export const AuthProvider = ({ children }) => {
+  const history = useHistory();
+  const [currentUser, setCurrentUser] = useState();
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   const login = () => {
     console.log("これはログインの関数です。");
   };
 
+  const getUserData = () => {
+    axios
+      .get("http://localhost:3001/logged_in", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        if (res.data.logged_in) {
+          setIsSignedIn(true);
+          setCurrentUser(res.data.user);
+          console.log("データ取得できました");
+          console.log(res.data.user);
+        } else {
+          history.push("/login");
+        }
+      });
+  };
+
   const value = {
+    currentUser,
+    setCurrentUser,
+    setIsSignedIn,
+    isSignedIn,
     login,
+    getUserData,
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
