c0897bfc9fba8db6e16d98bc183f61fad3323f3a
RYOTA <kobaryo04ysh@icloud.com>
2021-05-13 12:44:23 +0900

    fix: rubocopでコードを修正

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,9 @@
-class User < ApplicationRecord
-    has_secure_password
+# frozen_string_literal: true
 
-    validates :email, presence: true
-    validates :email, uniqueness: true
-    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
