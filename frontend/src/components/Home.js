e8e3bbb1e963ac82f534e0953d8f2255618e11bf
RYOTA <kobaryo04ysh@icloud.com>
2021-05-13 12:02:58 +0900

    fix: headerのユーザー名の部分をdropdpwnに変更

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import "../assets/Home.css";
 import axios from "axios";
 
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+
 const Home = (props) => {
   const history = useHistory();
   const { currentUser } = useAuth();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleLogoutClick = () => {
     axios
@@ -29,18 +44,32 @@ const Home = (props) => {
             <p className="header-logo">LOGO</p>
           </div>
           <div className="header-right">
-            <p>
-              こんにちは
-              <span className="header-username">
-                {currentUser && currentUser.email}
-              </span>
-              さん
-            </p>
+            <div>
+              <Button
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <span className="header-username">
+                  {currentUser && currentUser.email} <ArrowDropDownIcon />
+                </span>
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
+              </Menu>
+            </div>
           </div>
         </div>
       </header>
       <h1>Home</h1>
-      <button onClick={handleLogoutClick}>ログアウト</button>
     </div>
   );
 };
