e6d52f7cca5676ae1f891ce50e36ba9d3cb6243a
RYOTA <kobaryo04ysh@icloud.com>
2021-05-19 18:46:19 +0900

    fix: assetsをstylesに変更

--- frontend/src/components/atoms/Header.jsx
+++ frontend/src/components/atoms/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
-import "../../assets/Home.css";
+import "../../styles/Home.css";
 
 import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
