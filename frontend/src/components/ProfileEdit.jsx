02deb94383117a226242df506d636b30c20c448c
RYOTA <kobaryo04ysh@icloud.com>
2021-05-22 11:44:29 +0900

    feat: 現在のユーザーデータをformに反映

--- frontend/src/components/ProfileEdit.jsx
+++ frontend/src/components/ProfileEdit.jsx
@@ -1,36 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
-
+import "../styles/ProfileEdit.css";
 import TextField from "@material-ui/core/TextField";
 
 const ProfileEdit = () => {
   const { currentUser } = useAuth();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = () => {
     console.log("submit");
   };
 
+  useEffect(() => {
+    if (currentUser) {
+      setEmail(currentUser.email);
+      setUsername(currentUser.username);
+    }
+  }, []);
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <TextField
-          id="outlined-basic"
-          label="名前"
-          variant="outlined"
-          type="name"
-          name="name"
-          value={currentUser && currentUser.username}
-          // onChange={(event) => setUsername(event.target.value)}
-        />
-        <TextField
-          id="outlined-basic"
-          label="名前"
-          variant="outlined"
-          type="name"
-          name="name"
-          value={currentUser && currentUser.email}
-          //   onChange={(event) => setUsername(event.target.value)}
-        />
+        <div className="edit-form">
+          <TextField
+            id="outlined-basic"
+            label="名前"
+            variant="outlined"
+            type="name"
+            name="name"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="メールアドレス"
+            variant="outlined"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
       </form>
     </div>
   );
