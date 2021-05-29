366317c7b2df731513da674025d8707888da4fa9
RYOTA <kobaryo04ysh@icloud.com>
2021-05-29 12:01:31 +0900

    feat: tsの追加

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
-import React, { useContext, useState } from "react";
-import axios from "axios";
-import { useHistory } from "react-router-dom";
+import React, { useContext, useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const AuthContext = React.createContext({});
 
@@ -8,28 +8,27 @@ export const useAuth = () => {
   return useContext(AuthContext);
 };
 
-export const AuthProvider = ({ children }) => {
+export const AuthProvider: React.FC = ({ children }) => {
   const history = useHistory();
   const [currentUser, setCurrentUser] = useState();
   const [isSignedIn, setIsSignedIn] = useState(false);
 
   const login = () => {
-    console.log("これはログインの関数です。");
+    console.log('これはログインの関数です。');
   };
 
   const getUserData = () => {
     axios
-      .get("http://localhost:3001/logged_in", {
+      .get('http://localhost:3001/logged_in', {
         withCredentials: true,
       })
       .then((res) => {
         if (res.data.logged_in) {
           setIsSignedIn(true);
           setCurrentUser(res.data.user);
-          console.log("データ取得できました");
-          // console.log(res.data.user);
+          console.log('データ取得できました');
         } else {
-          history.push("/login");
+          history.push('/login');
         }
       });
   };
