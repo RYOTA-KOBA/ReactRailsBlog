aa931238d7822b410a098d05d440313d38558849
RYOTA <kobaryo04ysh@icloud.com>
2021-05-31 12:04:08 +0900

    feat: index, create, strong paramsを追加

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to action: 'index'
+    end
+      
+    private
+
+        def post_params
+            params.require(:post).permit(:title, :content)
+        end
 end
