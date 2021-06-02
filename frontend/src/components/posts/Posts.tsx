68c9a36f5fba9708d11658312d88d06e8db20a58
RYOTA <kobaryo04ysh@icloud.com>
2021-06-02 20:27:38 +0900

    feat: Cardの追加

--- frontend/src/components/posts/Posts.tsx
+++ frontend/src/components/posts/Posts.tsx
@@ -1,5 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../../styles/posts/Post.css';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 type D = {
   id: string;
@@ -11,6 +33,7 @@ type D = {
 
 const Posts: React.FC = () => {
   const [postsData, setPostsData] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     axios
@@ -33,10 +56,22 @@ const Posts: React.FC = () => {
       <h1>こんにちは</h1>
       {postsData &&
         postsData.map((data: D) => (
-          <ul key={data.id}>
-            <li>{data.title}</li>
-            <li>{data.content}</li>
-          </ul>
+          <Card className="post-card" variant="outlined" key={data.id}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                {data.title}
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                adjective
+              </Typography>
+              <Typography variant="body2" component="p">
+                {data.content}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
         ))}
     </div>
   );
