7f1bb1e9cda6a192fa4ec4b70ed7a57f714100ab
RYOTA <kobaryo04ysh@icloud.com>
2021-06-18 12:53:35 +0900

    fix: postsにuidを追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_023903) do
+ActiveRecord::Schema.define(version: 2021_06_18_033711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_31_023903) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "uid"
   end
 
   create_table "users", force: :cascade do |t|
